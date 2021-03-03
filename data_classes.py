a10880a090fed0bd0a55c1d771018393fce9e725
Ilya Vlasov <ilyavlasov2011@gmail.com>
2021-03-03 11:00:37 +0300

    Added grouped print

---
+++ data_classes.py
@@ -0,0 +1,27 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Category:
+    name: str
+    code_prefix: str
+    code_boundaries: List[int]
+
+
+@dataclass
+class Violation:
+    file_path: str
+    line: int
+    column: int
+    code_prefix: str
+    code: int
+    description: str
+
+    def __str__(self):
+        return f"{self.file_path}:{self.line}:{self.column} {self.code_prefix}{self.code} {self.description}"
+
+    def __post_init__(self):
+        self.line = int(self.line)
+        self.column = int(self.column)
+        self.code = int(self.code)
--- script.py
+++ script.py
@@ -1,20 +1,9 @@
+import json
 import re
 import subprocess
 import sys
-from dataclasses import dataclass
-from typing import List
 
-
-@dataclass
-class Violation:
-    file_path: str
-    line: int
-    column: int
-    violation_code: str
-    description: str
-
-    def __str__(self):
-        return f"{self.file_path}:{self.line}:{self.column} {self.violation_code} {self.description}"
+from data_classes import *
 
 
 def parse(*file_paths: str) -> List[Violation]:
@@ -23,7 +12,8 @@ def parse(*file_paths: str) -> List[Violation]:
     violations = []
 
     for line in map(str, output.stdout.splitlines()):
-        pattern = re.compile(r"""
+        violation_pattern = re.compile(
+            r"""
         (?P<file_path>.*) # До первой ":" находится путь к файлу
         [:]
         (?P<line>\d+) # После первой ":" идет номер строки
@@ -31,26 +21,67 @@ def parse(*file_paths: str) -> List[Violation]:
         (?P<column>\d+) # Далее, номер столбца
         [:]
         \s 
-        (?P<violation_code>[\w]+) # Код нарушения отделён пробелами 
+        (?P<code_prefix>\w+) # Код (включая префикс) нарушения отделён пробелами
+        (?P<code>\d{3}) # Код нарушения содержит ровно 3 цифры
         \s
         (?P<description>.*) # Всё что идёт после кода нарушения является описанием ошибки
-        """, re.VERBOSE)
+        """,
+            re.VERBOSE,
+        )
 
-        if not pattern.search(line):
+        if not violation_pattern.search(line):
             break
 
-        result = pattern.search(line).groupdict()
-        violation = Violation(**result)
+        groups = violation_pattern.search(line).groupdict()
+        violation = Violation(**groups)
         violations.append(violation)
 
     return violations
 
 
-def script(*file_paths: str):
-    violations = parse(*file_paths)
-
+def print_with_numbers(violations: List[Violation]):
     for i, violation in enumerate(violations, 1):
         print(f"{i}\t{violation}")
+
+
+def print_category(category_name: str, violations: List[Violation], start_index: int):
+    current_index = start_index
+    if violations:
+        print(category_name)
+        for elem in violations:
+            print(f"{current_index}\t{elem}")
+            current_index += 1
+        print()
+
+
+def print_grouped(violations: List[Violation]):
+    with open("categories.json") as f:
+        categories: List[Category] = json.load(f, object_hook=lambda items: Category(**items))
+
+    remaining: List[Violation] = violations
+
+    current_index: int = 1
+    for category in categories:
+        code_prefix: str = category.code_prefix
+        code_boundaries: List[int] = category.code_boundaries
+        filtered_violations = list(
+            filter(
+                lambda violation: violation.code_prefix == code_prefix and violation.code in range(*code_boundaries),
+                remaining,
+            )
+        )
+        remaining = [x for x in remaining if x not in filtered_violations]
+
+        print_category(category.name, filtered_violations, current_index)
+        current_index += len(filtered_violations)
+
+    print_category("Others", remaining, current_index)
+
+
+def script(*file_paths: str):
+    violations = parse(*file_paths)
+    # print_with_numbers(violations)
+    print_grouped(violations)
 
 
 if __name__ == "__main__":
